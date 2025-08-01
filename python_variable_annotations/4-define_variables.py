a5782402082feb9c476202b68f814a58d6824717
nawfel83 <nawfel83@example.com>
2025-08-01 18:30:14 +0200

    Add initial implementation of variable definitions and main test script

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for defining annotated variables."""
+
+a: int = 1
+"""An integer with a value of 1."""
+
+pi: float = 3.14
+"""A float with a value of 3.14."""
+
+i_understand_annotations: bool = True
+"""A boolean indicating understanding of annotations."""
+
+school: str = "Holberton"
+"""A string with the value 'Holberton'."""
