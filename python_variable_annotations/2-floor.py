09b22fc0c88215a8174b3bec65f20ff5048537c3
nawfel83 <nawfel83@example.com>
2025-08-01 18:29:54 +0200

    Add initial implementation of floor function and main test script

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for getting the floor of a float."""
+
+import math
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
+    return math.floor(n)
---
+++ python_variable_annotations/2-main.py
@@ -0,0 +1,13 @@
+# python_variable_annotations/2-main.py
+#!/usr/bin/env python3
+"""Main file for testing floor function."""
+
+import math
+
+floor = __import__('2-floor').floor
+
+ans = floor(3.14)
+
+print(ans == math.floor(3.14))
+print(floor.__annotations__)
+print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
