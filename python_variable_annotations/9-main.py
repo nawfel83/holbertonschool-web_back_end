70697a78726bcf8220daf1ea1d90e029e7529c07
nawfel83 <nawfel83@example.com>
2025-08-01 18:31:03 +0200

    Add initial implementation of element_length function and main test script

---
+++ python_variable_annotations/9-main.py
@@ -0,0 +1,7 @@
+# python_variable_annotations/9-main.py
+#!/usr/bin/env python3
+"""Main file for testing element_length function."""
+
+element_length =  __import__('9-element_length').element_length
+
+print(element_length.__annotations__)
