5cfb3b85e5f98990e1a97c14ddd416cc190537dc
nawfel83 <nawfel83@example.com>
2025-08-01 18:30:32 +0200

    Add initial implementation of sum_mixed_list function and main test script

---
+++ python_variable_annotations/6-main.py
@@ -0,0 +1,11 @@
+# python_variable_annotations/6-main.py
+#!/usr/bin/env python3
+"""Main file for testing sum_mixed_list function."""
+
+sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
+
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
