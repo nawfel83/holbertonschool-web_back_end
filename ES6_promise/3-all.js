efc0c36632827b0bd09591a8c563a443ce210532
nawfel83 <laklitnawfel1@gmail.com>
2025-08-29 20:34:00 +0200

    feat: implement handleProfileSignup function for user signup process

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+	return Promise.all([uploadPhoto(), createUser()])
+	.then(([result1, result2]) => {
+		 console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
+	})
+	.catch(() => {
+		console.log("Signup system offline");
+	});
+}
