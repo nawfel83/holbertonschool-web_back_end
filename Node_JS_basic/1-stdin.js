8fb9ad0258f017cd5ff38b048f9028233b88f485
nawfel83 <laklitnawfel1@gmail.com>
2025-09-05 21:15:40 +0200

    feat: add stdin handling to greet user and log input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
